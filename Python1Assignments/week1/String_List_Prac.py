93590fe0ef3d03aeaf3bc59fd52d0d863fa388cf
joeygrimm <joeygrimm86@gmail.com>
2017-09-05 20:54:30 -0500

    week 1 assignment added

--- Python1Assignments/week1/String_List_Prac.py
+++ Python1Assignments/week1/String_List_Prac.py
@@ -16,8 +16,8 @@ x = [19,2,54,-2,7,12,98,32,10,-3,6]
 print x
 x.sort()
 print x
-first_list = x[:len(x)/2] # optional first parameter of slice defaults to zero
-second_list = x[len(x)/2:] # optional second parameter of slice defaults to the list's length
+first_list = x[:len(x)/2] # optional first parameter of slice defaults to zero....I'm still confused on how this works
+second_list = x[len(x)/2:] # optional second parameter of slice defaults to the list's length.....same here
 print "first list", first_list
 print "second_list", second_list
 second_list.insert(0,first_list)
